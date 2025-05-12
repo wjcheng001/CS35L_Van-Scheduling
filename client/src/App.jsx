ebaea4a5b89a7895830d0eedee0505874b4e3f70
Zhaomeng Chen <zkao@Zhaomengs-MacBook-Pro.local>
2025-05-12 13:23:57 -0700

    Implement google login through with basic @g.ucla.edu verification

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,47 @@
 import { useEffect, useState } from 'react';
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from "jwt-decode";
 
 function App() {
   const [msg, setMsg] = useState('');
+  const [user,setUser] = useState(null)
 
-  useEffect(() => {
-    fetch('/api/test')
-      .then((res) => res.json())
-      .then((data) => setMsg(data.message))
-      .catch((err) => console.error('API error:', err));
-  }, []);
+  // useEffect(() => {
+  //   fetch('/api/test')
+  //     .then((res) => res.json())
+  //     .then((data) => setMsg(data.message))
+  //     .catch((err) => console.error('API error:', err));
+  // }, []);
 
-  return <h1>{msg || "Loading..."}</h1>;
+  return (
+    <div>
+      <h1>{msg || "Loading..."}</h1>
+
+      {!user ? (
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            const decoded = jwtDecode(credentialResponse.credential);
+            console.log("User info:", decoded);
+
+            if (decoded.email && decoded.email.endsWith('@g.ucla.edu')) {
+              setUser(decoded);
+              setError('');
+            } else {
+              setError('Only @g.ucla.edu email addresses are allowed.');
+            }
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
+      ) : (
+        <div>
+          <p>Welcome, {user.name}!</p>
+          <img src={user.picture} alt="profile" />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import secrets from './secrets.js';
+
+const clientId = secrets.clientId;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
-)
+);
