3c90748a5808093541fb2f5271bf12eac63b4434
Shluo03 <shennnnluo@gmail.com>
2025-05-17 03:32:50 +0800

    Update secrets.js

--- client/secrets.js
+++ client/secrets.js
@@ -1,2 +1,6 @@
-
 export const BACKEND_URL = 'http://localhost:5000';
+export const MAPBOX_TOKEN = 'your-mapbox-token-here';
+export const FEATURE_FLAGS = {
+  enableScheduling: true,
+  showBetaUI: false,
+};
