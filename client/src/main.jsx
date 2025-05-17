d9a35b75a475b50d908290562fa908ca4b827163
Cheng <chengwanjunri2014@gmail.com>
2025-05-16 23:27:34 -0700

    Now able to communicate with secrets.js

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import secrets from './secrets.js';
+import secrets from '../secrets.js';
 
 const clientId = secrets.clientId;
 
